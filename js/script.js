a7d72ba38a23fd1e3b5d8d71373929b0859a6d2e
AngelikaUrusova <129684308+AngelikaUrusova@users.noreply.github.com>
2023-04-04 05:12:57 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const title = document.querySelector('.title');
+const aEL = document.querySelector('.enter');
+const blockEl = document.querySelector('.login');
+
+aEL.addEventListener('click', function (e) {blockEl.classList.toggle('visible')})
