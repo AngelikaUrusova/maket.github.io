5508f0cb5b2699f6803403a1f2a0b3cc5f475ebf
AngelikaUrusova <129684308+AngelikaUrusova@users.noreply.github.com>
2023-05-12 23:37:20 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-const title = document.querySelector('.title');
-const aEL = document.querySelector('.enter');
-const blockEl = document.querySelector('.login');
+const popupBg = document.querySelector('.popup__bg');
+const popup = document.querySelector('.popup');
+const openPopup = document.querySelector('.enter');
+const closePopup = document.querySelector('.close_popup');
 
-aEL.addEventListener('click', function (e) {blockEl.classList.toggle('visible')})
+openPopup.addEventListener('click', (e) => {
+    e.preventDefault();
+    popupBg.classList.add('active');
+    popup.classList.add('active');
+})
+
+closePopup.addEventListener('click', () => {
+    popupBg.classList.remove('active');
+    popup.classList.remove('active');
+})
+
+
+
+
+
+const burgerEl = document.querySelector('.burger');
+const menuEl = document.querySelector('.burger__menu');
+
+burgerEl.addEventListener('click', function () {
+  menuEl.classList.toggle('menu_show');
+});
